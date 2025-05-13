40125983b3310728dabf3684bd279d50cbbf5179
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-13 14:15:44 -0400

    updates

--- include/utils/ShaderUtils.hpp
+++ include/utils/ShaderUtils.hpp
@@ -2,16 +2,20 @@
 
 #include <string>
 #include <vector>
+#include <vulkan/vulkan_core.h>
 
 class ShaderUtils {
   public:
-	ShaderUtils() = default;
+	ShaderUtils(VkDevice &device);
 	ShaderUtils(ShaderUtils &&) = default;
 	ShaderUtils(const ShaderUtils &) = default;
-	ShaderUtils &operator=(ShaderUtils &&) = default;
-	ShaderUtils &operator=(const ShaderUtils &) = default;
+	ShaderUtils &operator=(ShaderUtils &&) = delete;
+	ShaderUtils &operator=(const ShaderUtils &) = delete;
 	~ShaderUtils() = default;
 
   private:
+	VkDevice &device;
+
 	std::vector<char> readFile(const std::string &filename);
+	VkShaderModule createShaderModule(const std::vector<char> &binary);
 };
--- src/utils/ShaderUtils.cpp
+++ src/utils/ShaderUtils.cpp
@@ -1,6 +1,8 @@
 #include "utils/ShaderUtils.hpp"
 #include <fstream>
 
+ShaderUtils::ShaderUtils(VkDevice &device) : device(device) {}
+
 std::vector<char> ShaderUtils::readFile(const std::string &filename) {
 	std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
@@ -17,4 +19,18 @@ std::vector<char> ShaderUtils::readFile(const std::string &filename) {
 	file.close();
 
 	return buffer;
+}
+
+VkShaderModule ShaderUtils::createShaderModule(const std::vector<char> &binary) {
+	VkShaderModuleCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	createInfo.codeSize = binary.size();
+	createInfo.pCode = reinterpret_cast<const uint32_t *>(binary.data());
+
+	VkShaderModule shaderModule;
+	if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create shader module!");
+	}
+
+	return shaderModule;
 }
