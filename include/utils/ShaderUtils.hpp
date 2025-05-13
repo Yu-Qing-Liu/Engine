9fed1b32f6e5bbaa74bba505453e49cf7d7490d3
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-13 15:02:49 -0400

    updates

--- include/utils/ShaderUtils.hpp
+++ include/utils/ShaderUtils.hpp
@@ -11,16 +11,44 @@ class ShaderUtils {
   public:
 	ShaderUtils(VkDevice &device);
 	ShaderUtils(ShaderUtils &&) = default;
-	ShaderUtils(const ShaderUtils &) = default;
+	ShaderUtils(const ShaderUtils &) = delete;
 	ShaderUtils &operator=(ShaderUtils &&) = delete;
 	ShaderUtils &operator=(const ShaderUtils &) = delete;
 	~ShaderUtils() = default;
 
+	struct ShaderModules {
+		VkShaderModule vertex_shader;
+		VkShaderModule fragment_shader;
+		VkShaderModule geometry_shader;
+		VkShaderModule compute_shader;
+		VkShaderModule tesselation_shader;
+		VkShaderModule mesh_shader;
+	};
+
+	ShaderModules compileShaderProgram(std::string &shader_source_root_dir);
+
   private:
+	struct ShaderBinaries {
+		std::vector<uint32_t> vertex_shader;
+		std::vector<uint32_t> fragment_shader;
+		std::vector<uint32_t> geometry_shader;
+		std::vector<uint32_t> compute_shader;
+		std::vector<uint32_t> tesselation_shader;
+		std::vector<uint32_t> mesh_shader;
+	};
+
 	VkDevice &device;
 
-	std::vector<SpvCompilationResult> compileShader(std::vector<std::string> shader_paths);
+	Compiler compiler;
+	CompileOptions options;
 
-	std::vector<char> readFile(const std::string &filename);
+	std::string shader_root_path = std::string(PROJECT_ROOT_DIR) + "/src/shaders";
+	std::string shader_cache_path = std::string(PROJECT_ROOT_DIR) + "/src/cache";
+
+	ShaderBinaries compileShader(const std::vector<std::string> &shader_paths);
+	std::vector<uint32_t> compileShader(const std::string &shader_path);
+
+	void createDirectory(const std::string &path);
+	std::string readFile(const std::string &file_path);
 	VkShaderModule createShaderModule(const std::vector<char> &binary);
 };
--- src/utils/ShaderUtils.cpp
+++ src/utils/ShaderUtils.cpp
@@ -1,24 +1,69 @@
 #include "utils/ShaderUtils.hpp"
+#include <filesystem>
 #include <fstream>
+#include <iostream>
+#include <shaderc/shaderc.h>
+#include <shaderc/shaderc.hpp>
+#include <string>
+#include <vector>
 
-ShaderUtils::ShaderUtils(VkDevice &device) : device(device) {}
+using namespace std::filesystem;
 
-std::vector<char> ShaderUtils::readFile(const std::string &filename) {
-	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+ShaderUtils::ShaderUtils(VkDevice &device) : device(device) {
+	createDirectory(shader_root_path);
+	createDirectory(shader_cache_path);
+}
 
-	if (!file.is_open()) {
-		throw std::runtime_error("failed to open file!");
+// [.../xxx.vert, .../xxx.frag, .../xxx.geom, ...]
+ShaderUtils::ShaderBinaries ShaderUtils::compileShader(const std::vector<std::string> &shader_paths) {
+	ShaderBinaries binaries;
+	for (const auto &p : shader_paths) {
+		std::string extension = path(p).extension().string();
+
+		if (extension == ".vert") {
+		}
 	}
+	return binaries;
+}
 
-	size_t fileSize = (size_t)file.tellg();
-	std::vector<char> buffer(fileSize);
+std::vector<uint32_t> ShaderUtils::compileShader(const std::string &shader_path) {
+	std::string shader_code = readFile(shader_path);
+	SpvCompilationResult result = compiler.CompileGlslToSpv(shader_code, shaderc_glsl_vertex_shader, shader_path.c_str());
+	if (result.GetCompilationStatus() != shaderc_compilation_status_success) {
+		std::cerr << "Failed to compile shader: " << shader_path << std::endl;
+		std::cerr << "Error: " << result.GetErrorMessage() << std::endl;
+        return {};
+	} 
+    std::vector<uint32_t> spirv(result.cbegin(), result.cend());
+    return spirv;
+}
 
-	file.seekg(0);
-	file.read(buffer.data(), fileSize);
+void ShaderUtils::createDirectory(const std::string &path) {
+	if (!exists(path)) {
+		try {
+			if (create_directory(path)) {
+				std::cout << "Directory created successfully: " << path << std::endl;
+			} else {
+				std::cerr << "Failed to create directory: " << path << std::endl;
+			}
+		} catch (const filesystem_error &e) {
+			std::cerr << "Error while creating directory: " << e.what() << std::endl;
+		}
+	} else {
+		std::cout << "Directory already exists: " << path << std::endl;
+	}
+}
 
-	file.close();
-
-	return buffer;
+std::string ShaderUtils::readFile(const std::string &file_path) {
+	std::ifstream shader_file(file_path);
+	if (!shader_file.is_open()) {
+		std::cerr << "Failed to open shader file: " << file_path << std::endl;
+		return "";
+	}
+	std::stringstream buffer;
+	buffer << shader_file.rdbuf();
+	std::string content = buffer.str();
+	return content;
 }
 
 VkShaderModule ShaderUtils::createShaderModule(const std::vector<char> &binary) {
