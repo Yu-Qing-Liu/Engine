237e5dce1c480c623dcac426690db525dcfe9497
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-05-26 09:58:08 -0400

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -636,24 +636,6 @@ class Application {
 
         triangleModel->draw(commandBuffer);
 
-		/* vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline); */
-
-		/* VkViewport viewport{}; */
-		/* viewport.x = 0.0f; */
-		/* viewport.y = 0.0f; */
-		/* viewport.width = (float)swapChainExtent.width; */
-		/* viewport.height = (float)swapChainExtent.height; */
-		/* viewport.minDepth = 0.0f; */
-		/* viewport.maxDepth = 1.0f; */
-		/* vkCmdSetViewport(commandBuffer, 0, 1, &viewport); */
-
-		/* VkRect2D scissor{}; */
-		/* scissor.offset = {0, 0}; */
-		/* scissor.extent = swapChainExtent; */
-		/* vkCmdSetScissor(commandBuffer, 0, 1, &scissor); */
-
-		/* vkCmdDraw(commandBuffer, 3, 1, 0, 0); */
-
 		vkCmdEndRenderPass(commandBuffer);
 
 		if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
