52c8d55b79a2130f32647a293ebe2d531446c418
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-07-09 11:36:11 -0400

    scenes

--- include/models/implementations/triangle.hpp
+++ include/models/implementations/triangle.hpp
@@ -12,6 +12,4 @@ class Triangle : public Model {
 	~Triangle() = default;
 
 	void draw(VkCommandBuffer &commandBuffer, const vec3 &position = vec3(0.0f, 0.0f, 0.0f), const quat &rotation = quat(), const vec3 &scale = vec3(1.0f, 1.0f, 1.0f), const vec3 &color = vec3(1.0f, 1.0f, 1.0f)) override;
-
-  private:
 };
