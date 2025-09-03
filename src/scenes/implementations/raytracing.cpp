67b0e349ad72e855d983f5e14138f7eec6a2742e
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-09-03 13:25:57 -0400

    updates
    updates

--- src/scenes/implementations/raytracing.cpp
+++ src/scenes/implementations/raytracing.cpp
@@ -8,13 +8,30 @@
 
 RayTracing::RayTracing(Scenes &scenes) : Scene(scenes) {
     this->cube = make_unique<Polygon>(
-        std::vector<Model::Vertex> {
-            {{0.0f, 0.5f, 0.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-            {{-0.433f, -0.25f, 0.0f}, {0.0f, 1.0f, 0.0f, 1.0f}},
-            {{0.433f, -0.25f, 0.0f}, {0.0f, 0.0f, 1.0f, 1.0f}},
+        std::vector<Model::Vertex>{
+            // idx, position                 // color (RGBA)
+            /*0*/ {{-0.5f, -0.5f, -0.5f},     {1.0f, 0.0f, 0.0f, 1.0f}}, // LBB
+            /*1*/ {{ 0.5f, -0.5f, -0.5f},     {0.0f, 1.0f, 0.0f, 1.0f}}, // RBB
+            /*2*/ {{ 0.5f,  0.5f, -0.5f},     {0.0f, 0.0f, 1.0f, 1.0f}}, // RTB
+            /*3*/ {{-0.5f,  0.5f, -0.5f},     {1.0f, 1.0f, 0.0f, 1.0f}}, // LTB
+            /*4*/ {{-0.5f, -0.5f,  0.5f},     {1.0f, 0.0f, 1.0f, 1.0f}}, // LBF
+            /*5*/ {{ 0.5f, -0.5f,  0.5f},     {0.0f, 1.0f, 1.0f, 1.0f}}, // RBF
+            /*6*/ {{ 0.5f,  0.5f,  0.5f},     {1.0f, 1.0f, 1.0f, 1.0f}}, // RTF
+            /*7*/ {{-0.5f,  0.5f,  0.5f},     {0.0f, 0.0f, 0.0f, 1.0f}}, // LTF
         },
-        std::vector<uint16_t> {
-            0, 1, 2
+        std::vector<uint16_t>{
+            // Front  (+Z)
+            4, 5, 6,   6, 7, 4,
+            // Back   (-Z)
+            1, 0, 3,   3, 2, 1,
+            // Left   (-X)
+            0, 4, 7,   7, 3, 0,
+            // Right  (+X)
+            5, 1, 2,   2, 6, 5,
+            // Top    (+Y)
+            3, 7, 6,   6, 2, 3,
+            // Bottom (-Y)
+            0, 1, 5,   5, 4, 0,
         }
     );
 }
@@ -39,7 +56,7 @@ void RayTracing::computePass() {
 }
 
 void RayTracing::updateUniformBuffers() {
-    cube->updateUniformBuffer();
+    cube->updateUniformBuffer(rotate(mat4(1.0f), Engine::time * radians(90.0f), vec3(0.0f, 0.0f, 1.0f)));
 }
 
 void RayTracing::swapChainUpdate() {
