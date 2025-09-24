c31722caed08f53151a03971494bc14a1fddefeb
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-09-24 13:40:23 -0400

    merge

--- src/scenes/scenes.cpp
+++ src/scenes/scenes.cpp
@@ -1,10 +1,14 @@
 #include "scenes.hpp"
-#include "graph.hpp"
-#include "overlay.hpp"
+#include "buttons.hpp"
+#include "default.hpp"
+#include "instancing.hpp"
+#include "raytracing.hpp"
 
 Scenes::Scenes() {
-    scenesContainer.emplace_back(make_shared<Graph>(*this));
-    scenesContainer.emplace_back(make_shared<Overlay>(*this));
+    scenesContainer.emplace_back(make_shared<Default>(*this));
+    scenesContainer.emplace_back(make_shared<Buttons>(*this));
+    scenesContainer.emplace_back(make_shared<Instancing>(*this));
+    scenesContainer.emplace_back(make_shared<RayTracing>(*this));
     for (const auto &sc : scenesContainer) {
         scenes[sc->getName()] = {sc, true};
     }
